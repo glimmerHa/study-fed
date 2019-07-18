d17eda429e0fc4ec3fbd1ae9bba157668eb6e6bf
glimmerHa <glimmerHa@outlook.com>
2019-07-18 20:51:37 +0800

    准备工作，element -admin 代码结构学习，并修改成个人学习记录

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Layout from '@/views/layout/Layout'
+
+Vue.use(Router)
+
+const router = new Router({
+    mode: 'history',
+    // routes: routes
+})
+
+export default router
